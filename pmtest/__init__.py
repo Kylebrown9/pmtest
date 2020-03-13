183586c1dc62d7dac46564912a7f6cf9e119a17c
Kyle Brown <kyleb@pop-os.localdomain>
2020-03-12 20:13:11 -0400

    Initial commit

---
+++ pmtest/__init__.py
@@ -0,0 +1,5 @@
+import os
+from pm4py.objects.log.importer.csv import factory as csv_importer
+
+event_stream = csv_importer.import_event_stream(
+    os.path.join("tests", "input_data", "running-example.csv"))
